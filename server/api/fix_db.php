4e839265828c5cc27b72b8cfbe7c257b7af9f627
MiniMines Associate <itassociate@m-mines.com>
2026-09-25 16:51:09 +0530

    remove clone

--- server/api/fix_db.php
+++ server/api/fix_db.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../config/db.php';
-$db = clone (new Database())->getConnection();
+$db = (new Database())->getConnection();
 $db->query("DELETE FROM system_settings WHERE setting_key = 'rules'");
 echo 'Fixed rules in DB';
 ?>
